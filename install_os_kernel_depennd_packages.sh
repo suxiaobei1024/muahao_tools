d48fcf6285973f26421c770d888cf1b90268b066
root <root@ip-10-169-17-220.idata-server.shopee.io>
2021-09-24 16:16:05 +0800

    update

---
+++ install_os_kernel_depennd_packages.sh
@@ -0,0 +1,11 @@
+yum groups mark install "Development Tools"
+yum groups mark convert "Development Tools"
+yum groupinstall "Development Tools" -y
+yum install pixman-devel -y
+yum install -y glib2-devel
+#安装ncurse-devel包 （make menuconfig 文本界面窗口依赖包）
+yum -y install ncurses-devel
+yum install automake autoconf libtool bison flex cmake crash -y
+
+yum install -y perl-ExtUtils-MakeMaker
+
